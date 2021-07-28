d2e73ce779b8941ff848f37b1e3be59e3190e709
Woensug Choi <woensug.choi@gmail.com>
2021-07-29 07:07:12 +0900

    cudagl base image suggestion

--- build.bash
+++ build.bash
@@ -32,8 +32,69 @@ then
     exit 1
 fi
 
+BUILD_BASE=""
+
+# If noetic (default)
+if [ "$1" == "noetic" ]
+then
+  BUILD_BASE="--build-arg BASEIMG=ros:noetic-ros-base \
+              --build-arg RELEASE=focal \
+              --build-arg DIST=noetic \
+              --build-arg GAZ=gazebo11"
+fi
+
+# If melodic (default)
+if [ "$1" == "melodic" ]
+then
+  BUILD_BASE="--build-arg BASEIMG=ros:melodic-ros-base \
+              --build-arg RELEASE=bionic \
+              --build-arg DIST=melodic \
+              --build-arg GAZ=gazebo9"
+fi
+
+# If kinetic (default)
+if [ "$1" == "kinetic" ]
+then
+  BUILD_BASE="--build-arg BASEIMG=ros:kinetic-ros-base \
+              --build-arg RELEASE=xenial \
+              --build-arg DIST=kinetic \
+              --build-arg GAZ=gazebo7"
+fi
+
+# Option key
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+case $2 in
+    -w|--without-nvidia)
+    if [ "$1" == "noetic" ]
+    then
+      BUILD_BASE="--build-arg BASEIMG=nvidia/cudagl:11.3.0-devel \
+                  --build-arg RELEASE=focal \
+                  --build-arg DIST=noetic \
+                  --build-arg GAZ=gazebo11"
+    fi
+    if [ "$1" == "melodic" ]
+    then
+      BUILD_BASE="--build-arg BASEIMG=nvidia/cudagl:11.3.0-devel-ubuntu18.04 \
+                  --build-arg RELEASE=focal \
+                  --build-arg DIST=noetic \
+                  --build-arg GAZ=gazebo11"
+    fi
+    echo "Building Docker image based on NVIDIA cudagl (CUDA + Opengl) base image"
+    shift
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1")
+    shift
+    ;;
+esac
+done
+
+echo $BUILD_BASE
+
 image_plus_tag=$image_name:$(export LC_ALL=C; date +%Y_%m_%d_%H%M)
-docker build --rm -t $image_plus_tag -f "${1}"/Dockerfile "${1}" && \
+docker build --rm -t $image_plus_tag -f "${1}"/Dockerfile $BUILD_BASE "${1}" && \
 docker tag $image_plus_tag $image_name:latest && \
 echo "Built $image_plus_tag and tagged as $image_name:latest"
 echo "To run:"
